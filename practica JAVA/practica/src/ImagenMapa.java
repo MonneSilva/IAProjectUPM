1708c1ab07bbc4093c3277826cf0aab98918cb8b
j18python <57668686+j18python@users.noreply.github.com>
2019-11-21 15:51:39 +0100

    map

--- practica JAVA/practica/src/ImagenMapa.java
+++ practica JAVA/practica/src/ImagenMapa.java
@@ -12,7 +12,7 @@ public class ImagenMapa extends JPanel {
 
 	public void paint(Graphics g) {
 		super.paint(g);
-		ImageIcon icono=new ImageIcon(getClass().getResource("/resources/metro_monterrey.png"));
+		ImageIcon icono=new ImageIcon(getClass().getResource("/resources/metrojapon.jpg"));
 		Image imagen=icono.getImage();
 		g.drawImage(imagen, 0, 0, getWidth(), getHeight(), this);
 		
