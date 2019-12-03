75a6eaa9659f1c1391eb3d8a9bf965b0f7d8f02d
Montserrat Silva <40004042+MonneSilva@users.noreply.github.com>
2019-12-03 15:30:31 +0100

    StarA working

--- practica JAVA/practica/src/EstacionesMonterrey.java
+++ practica JAVA/practica/src/EstacionesMonterrey.java
@@ -1,52 +1,52 @@
 public class EstacionesMonterrey{
 
 	//Linea Amarilla(1)
-	static AtributosEstacion Talleres = new AtributosEstacion("Talleres","San_Bernabé",null,false,"Amarilla",25.754019569730485, -100.36527872085571);
-	static AtributosEstacion San_Bernabé = new AtributosEstacion("San_Bernabé","Unidad_Modelo","Talleres",false,"Amarilla",25.74841482979281,-100.36171674728394);
-	static AtributosEstacion Unidad_Modelo = new AtributosEstacion("Unidad_Modelo","Aztlán","San_Bernabé",false,"Amarilla",25.74192073145334,-100.35480737686157);
-	static AtributosEstacion Aztlán = new AtributosEstacion("Aztlán","Penitenciaría","Unidad_Modelo",false,"Amarilla",25.732391545934494,-100.34738302230835);
-	static AtributosEstacion Penitenciaría = new AtributosEstacion("Penitenciaría","Alfonso_Reyes","Aztlán",false,"Amarilla",25.723267553136925,-100.34257650375366);
-	static AtributosEstacion Alfonso_Reyes = new AtributosEstacion("Alfonso_Reyes","Mitras","Penitenciaría",false,"Amarilla",25.716037451562844,-100.34259796142578);
-	static AtributosEstacion Mitras = new AtributosEstacion("Mitras","Simón_Bolívar","Alfonso_Reyes",false,"Amarilla",25.7060035077538,-100.3425121307373);
-	static AtributosEstacion Simón_Bolívar = new AtributosEstacion("Simón_Bolívar","Hospital","Mitras",false,"Amarilla",25.69906238496458,-100.3432846069336);
-	static AtributosEstacion Hospital = new AtributosEstacion("Hospital","Edison","Simón_Bolívar",false,"Amarilla",25.69215953006106,-100.3441858291626);
+	static AtributosEstacion Talleres = new AtributosEstacion("Talleres","San_Bernabï¿½",null,false,"Amarilla",25.754019569730485, -100.36527872085571);
+	static AtributosEstacion San_Bernabe = new AtributosEstacion("San_Bernabï¿½","Unidad_Modelo","Talleres",false,"Amarilla",25.74841482979281,-100.36171674728394);
+	static AtributosEstacion Unidad_Modelo = new AtributosEstacion("Unidad_Modelo","Aztlï¿½n","San_Bernabï¿½",false,"Amarilla",25.74192073145334,-100.35480737686157);
+	static AtributosEstacion Aztlan = new AtributosEstacion("Aztlï¿½n","Penitenciarï¿½a","Unidad_Modelo",false,"Amarilla",25.732391545934494,-100.34738302230835);
+	static AtributosEstacion Penitenciaria = new AtributosEstacion("Penitenciarï¿½a","Alfonso_Reyes","Aztlï¿½n",false,"Amarilla",25.723267553136925,-100.34257650375366);
+	static AtributosEstacion Alfonso_Reyes = new AtributosEstacion("Alfonso_Reyes","Mitras","Penitenciarï¿½a",false,"Amarilla",25.716037451562844,-100.34259796142578);
+	static AtributosEstacion Mitras = new AtributosEstacion("Mitras","Simï¿½n_Bolï¿½var","Alfonso_Reyes",false,"Amarilla",25.7060035077538,-100.3425121307373);
+	static AtributosEstacion Simon_Bolivar = new AtributosEstacion("Simï¿½n_Bolï¿½var","Hospital","Mitras",false,"Amarilla",25.69906238496458,-100.3432846069336);
+	static AtributosEstacion Hospital = new AtributosEstacion("Hospital","Edison","Simï¿½n_Bolï¿½var",false,"Amarilla",25.69215953006106,-100.3441858291626);
 	static AtributosEstacion Edison = new AtributosEstacion("Edison","Central","Hospital",false,"Amarilla",25.68715132605641,-100.33362865447998);
-	static AtributosEstacion Central= new AtributosEstacion("Central","Cuauhtémoc","Edison",false,"Amarilla",25.687228674437396,-100.32442331314087);
-	static AtributosEstacion Cuauhtémoc = new AtributosEstacion("Cuauhtémoc","Del_Golfo","Central",true,5,"Alameda","General_Anaya","Amarilla","Verde",25.68639717670977,-100.31732082366943);
-	static AtributosEstacion Del_Golfo = new AtributosEstacion("Del_Golfo","Felix_U_Gómez","Cuauhtémoc",false,"Amarilla",25.68514025052232,-100.30661344528198);
-	static AtributosEstacion Felix_U_Gómez = new AtributosEstacion("Felix_U_Gómez","Parque_Fundidora","Del_Golfo",true,5,"Adolfo_Prieto","Conchello","Amarilla","Rosa",25.68396066157605,-100.29680728912354);
-	static AtributosEstacion Parque_Fundidora = new AtributosEstacion("Parque_Fundidora","Y_Griega","Felix_U_Gómez",false,"Amarilla",25.68378662287394,-100.28822422027588);
+	static AtributosEstacion Central= new AtributosEstacion("Central","Cuauhtï¿½moc","Edison",false,"Amarilla",25.687228674437396,-100.32442331314087);
+	static AtributosEstacion Cuauhtemoc = new AtributosEstacion("Cuauhtï¿½moc","Del_Golfo","Central",true,5,"Alameda","General_Anaya","Amarilla","Verde",25.68639717670977,-100.31732082366943);
+	static AtributosEstacion Del_Golfo = new AtributosEstacion("Del_Golfo","Felix_U_Gï¿½mez","Cuauhtï¿½moc",false,"Amarilla",25.68514025052232,-100.30661344528198);
+	static AtributosEstacion Felix_U_Gomez = new AtributosEstacion("Felix_U_Gï¿½mez","Parque_Fundidora","Del_Golfo",true,5,"Adolfo_Prieto","Conchello","Amarilla","Rosa",25.68396066157605,-100.29680728912354);
+	static AtributosEstacion Parque_Fundidora = new AtributosEstacion("Parque_Fundidora","Y_Griega","Felix_U_Gï¿½mez",false,"Amarilla",25.68378662287394,-100.28822422027588);
 	static AtributosEstacion Y_Griega = new AtributosEstacion("Y_Griega","Eloy_Cavazos","Parque_Fundidora",false,"Amarilla",25.683341856146832,-100.27964115142822);
 	static AtributosEstacion Eloy_Cavazos = new AtributosEstacion("Eloy_Cavazos","Lerdo_de_Tejada","Y_Griega",false,"Amarilla",25.680093074914446,-100.2642560005188);
-	static AtributosEstacion Lerdo_de_Tejada = new AtributosEstacion("Lerdo_de_Tejada","Exposición","Eloy_Cavazos",false,"Amarilla",25.679822339146607,-100.25232553482056);
-	static AtributosEstacion Exposición = new AtributosEstacion("Exposición",null,"Lerdo_de_Tejada",false,"Amarilla",25.679532264427028,-100.24558782577515);
+	static AtributosEstacion Lerdo_de_Tejada = new AtributosEstacion("Lerdo_de_Tejada","Exposiciï¿½n","Eloy_Cavazos",false,"Amarilla",25.679822339146607,-100.25232553482056);
+	static AtributosEstacion Exposicion = new AtributosEstacion("Exposiciï¿½n",null,"Lerdo_de_Tejada",false,"Amarilla",25.679532264427028,-100.24558782577515);
 
 	//Linea Verde (2)
 	static AtributosEstacion Sendero = new AtributosEstacion("Sendero","Santiago_Tapia",null,false,"Verde",25.768629303392043,-100.29288053512573);
-	static AtributosEstacion Santiago_Tapia = new AtributosEstacion("Santiago_Tapia","San_Nicolás","Sendero",false,"Verde",25.759208550046573,-100.29569149017334);
-	static AtributosEstacion San_Nicolás = new AtributosEstacion("San_Nicolás","Anáhuac","Santiago_Tapia",false,"Verde",25.75272470503038,-100.29803037643433);
-	static AtributosEstacion Anáhuac = new AtributosEstacion("Anáhuac","Universidad","San_Nicolás",false,"Verde",25.739775281683535,-100.30272960662842);
-	static AtributosEstacion Universidad = new AtributosEstacion("Universidad","Niños_Héroes","Anáhuac",false,"Verde",25.724388759472287,-100.30824422836304);
-	static AtributosEstacion Niños_Héroes = new AtributosEstacion("Niños_Héroes","Regina","Universidad",false,"Verde",25.71702340038231,-100.31090497970581);
-	static AtributosEstacion Regina = new AtributosEstacion("Regina","General_Anaya","Niños_Héroes",false,"Verde",25.708284902625707,-100.3141450881958);
-	static AtributosEstacion General_Anaya = new AtributosEstacion("General_Anaya","Cuauhtémoc","Regina",false,"Verde",25.697167523466092,-100.31663417816162);
-	static AtributosEstacion Alameda = new AtributosEstacion("Alameda","Fundadores","Cuauhtémoc",false,"Verde",25.677056931431586,-100.31813621520996);
+	static AtributosEstacion Santiago_Tapia = new AtributosEstacion("Santiago_Tapia","San_Nicolï¿½s","Sendero",false,"Verde",25.759208550046573,-100.29569149017334);
+	static AtributosEstacion San_Nicolas = new AtributosEstacion("San_Nicolï¿½s","Anï¿½huac","Santiago_Tapia",false,"Verde",25.75272470503038,-100.29803037643433);
+	static AtributosEstacion Anahuac = new AtributosEstacion("Anï¿½huac","Universidad","San_Nicolï¿½s",false,"Verde",25.739775281683535,-100.30272960662842);
+	static AtributosEstacion Universidad = new AtributosEstacion("Universidad","Niï¿½os_Hï¿½roes","Anï¿½huac",false,"Verde",25.724388759472287,-100.30824422836304);
+	static AtributosEstacion Ninos_Heroes = new AtributosEstacion("Niï¿½os_Hï¿½roes","Regina","Universidad",false,"Verde",25.71702340038231,-100.31090497970581);
+	static AtributosEstacion Regina = new AtributosEstacion("Regina","General_Anaya","Niï¿½os_Hï¿½roes",false,"Verde",25.708284902625707,-100.3141450881958);
+	static AtributosEstacion General_Anaya = new AtributosEstacion("General_Anaya","Cuauhtï¿½moc","Regina",false,"Verde",25.697167523466092,-100.31663417816162);
+	static AtributosEstacion Alameda = new AtributosEstacion("Alameda","Fundadores","Cuauhtï¿½moc",false,"Verde",25.677056931431586,-100.31813621520996);
 	static AtributosEstacion Fundadores = new AtributosEstacion("Fundadores","Padre_Mier","Alameda",false,"Verde",25.672492901433582,-100.31911253929138);
 	static AtributosEstacion Padre_Mier = new AtributosEstacion("Padre_Mier","General_I_Zaragoza","Fundadores",false,"Verde",25.66860560248465,-100.31546473503113);
 	static AtributosEstacion General_I_Zaragoza = new AtributosEstacion("General_I_Zaragoza",null,"Padre_Mier",true,5,null,"Santa_Lucia","Verde","Rosa",25.667744964434828,-100.31023979187012);
 
 	//Linea Rosa(3)
-	static AtributosEstacion Hospital_Metropolitano = new AtributosEstacion("Hospital_Metropolitano","Los_Ángeles",null,false,"Rosa",25.7137923,-100.2732006);
-	static AtributosEstacion Los_Ángeles = new AtributosEstacion("Los_Ángeles","Ruíz_Cortines","Hospital_Metropolitano",false,"Rosa",25.708188234238758,-100.28140604496002);
-	static AtributosEstacion Ruíz_Cortines = new AtributosEstacion("Ruíz_Cortines","Violeta","Los_Ángeles",false,"Rosa",25.70370273473855,-100.28787553310394);
-	static AtributosEstacion Violeta = new AtributosEstacion("Violeta","Conchello","Ruíz_Cortines",false,"Rosa",25.7004216,-100.2928895);
-	static AtributosEstacion Conchello= new AtributosEstacion("Conchello","Felix_U_Gómez","Violeta",false,"Rosa",25.691054939460663,-100.29584035277367);
-	static AtributosEstacion Adolfo_Prieto = new AtributosEstacion("Adolfo_Prieto","Santa_Lucia","Felix_U_Gómez",false,"Rosa",25.677844982475868,-100.29763609170914);
+	static AtributosEstacion Hospital_Metropolitano = new AtributosEstacion("Hospital_Metropolitano","Los_ï¿½ngeles",null,false,"Rosa",25.7137923,-100.2732006);
+	static AtributosEstacion Los_angeles = new AtributosEstacion("Los_ï¿½ngeles","Ruï¿½z_Cortines","Hospital_Metropolitano",false,"Rosa",25.708188234238758,-100.28140604496002);
+	static AtributosEstacion Ruiz_Cortines = new AtributosEstacion("Ruï¿½z_Cortines","Violeta","Los_ï¿½ngeles",false,"Rosa",25.70370273473855,-100.28787553310394);
+	static AtributosEstacion Violeta = new AtributosEstacion("Violeta","Conchello","Ruï¿½z_Cortines",false,"Rosa",25.7004216,-100.2928895);
+	static AtributosEstacion Conchello= new AtributosEstacion("Conchello","Felix_U_Gï¿½mez","Violeta",false,"Rosa",25.691054939460663,-100.29584035277367);
+	static AtributosEstacion Adolfo_Prieto = new AtributosEstacion("Adolfo_Prieto","Santa_Lucia","Felix_U_Gï¿½mez",false,"Rosa",25.677844982475868,-100.29763609170914);
 	static AtributosEstacion Santa_Lucia = new AtributosEstacion("Santa_Lucia","General_I_Zaragoza","Adolfo_Prieto",false,"Rosa",25.672226983281032,-100.29855877161026);
 
-	static AtributosEstacion[] Paradas = {Adolfo_Prieto,Alameda,Alfonso_Reyes,Anáhuac,Aztlán,Central,Conchello,Cuauhtémoc,Del_Golfo,Edison,Eloy_Cavazos,Exposición,Felix_U_Gómez,
-		Fundadores,General_Anaya,General_I_Zaragoza,Hospital,Hospital_Metropolitano,Lerdo_de_Tejada,Los_Ángeles,Mitras,Niños_Héroes,Padre_Mier,Parque_Fundidora,Penitenciaría,
-		Regina,Ruíz_Cortines,San_Bernabé,San_Nicolás,Santa_Lucia,Santiago_Tapia,Sendero,Simón_Bolívar,Talleres,Unidad_Modelo,Universidad,Violeta,Y_Griega};
+	static AtributosEstacion[] Paradas = {Adolfo_Prieto,Alameda,Alfonso_Reyes,Anahuac,Aztlan,Central,Conchello,Cuauhtemoc,Del_Golfo,Edison,Eloy_Cavazos,Exposicion,Felix_U_Gomez,
+		Fundadores,General_Anaya,General_I_Zaragoza,Hospital,Hospital_Metropolitano,Lerdo_de_Tejada,Los_angeles,Mitras,Ninos_Heroes,Padre_Mier,Parque_Fundidora,Penitenciaria,
+		Regina,Ruiz_Cortines,San_Bernabe,San_Nicolas,Santa_Lucia,Santiago_Tapia,Sendero,Simon_Bolivar,Talleres,Unidad_Modelo,Universidad,Violeta,Y_Griega};
 	
 	static String[] horario = {"5:00-7:00","7:00-9:30*","9:30-13:00","13:00-15:00*","15:00-19:00","19:00-21:30*","21:30-23:00"};
 }
---
+++ practica JAVA/practica/src/Metro/Metro.java
@@ -0,0 +1,262 @@
+package Metro;
+
+import Graph.Edge;
+import Graph.Graph;
+import Graph.Node;
+import Search.StarA;
+import java.util.*;
+
+public class Metro {
+
+    Graph metro = new Graph();
+    ArrayList<Station> stations;
+    public ArrayList<Station> getStations() {
+        return stations;
+    }
+    public ArrayList<String> getStationsN() {
+        ArrayList S=new ArrayList();
+        for(Station s:this.stations)
+        {
+            S.add(s.getName());
+        }
+        return S;
+    }
+
+    public Graph getMetro() {
+        return metro;
+    }
+
+    public void setMetro(Graph metro) {
+        this.metro = metro;
+    }
+
+    public void setStations(ArrayList<Station> stations) {
+        this.stations = stations;
+    }
+    Station shinOkubo, takadanobaba, mejiro, ikebukuro, otsuka, sugamo, komagome, tabata, nishiNippori, nippori,
+            uguisudani, ueno, okachimachi, akihabara, kanda, tokyo, yurakucho, shimbashi, hamamatsucho, tamachi,
+            shinagawa, osaki, gotanda, merugo, ebisu, shibuya, harajuku, yoyogi, shinjuku, ochanomizu,
+            sendagaya, shinanomachi, yotsuya, iichigaya, iidabashi, suidobashi;
+
+    static String[] schedule = {"5:00-7:00", "7:00-9:30*", "9:30-13:00", "13:00-15:00*", "15:00-19:00", "19:00-21:30*", "21:30-23:00"};
+
+    public void createStations() {
+
+        //FIRST CREATE EACH STATION WHIT COORDANADES AND GRAPH COORDANADES*    *Optional
+        String line = "green";
+        //GREEN LINE
+        takadanobaba = new Station("Takadanobaba", 35.712932, 139.704455, line);
+        mejiro = new Station("Mejiro", 35.7211714, 139.7065603, line);
+        ikebukuro = new Station("Ikebukuro", 35.7295028, 139.7109001, line);
+        otsuka = new Station("Otsuka", 35.7318309, 139.7281112, line);
+        sugamo = new Station("Sugamo", 35.7334192, 139.7392848, line);
+        komagome = new Station("Komagome", 35.7365665, 139.7470098, line);
+        tabata = new Station("Tabata", 35.7381581, 139.7608154, line);
+        nishiNippori = new Station("Nishi-Nippori", 35.7320057, 139.7668856, line);
+        nippori = new Station("Nippori", 35.7281578, 139.7706414, line);
+        uguisudani = new Station("Uguisudani", 35.7214573, 139.7780133, line);
+        ueno = new Station("Ueno", 35.7141672, 139.7774091, line);
+        okachimachi = new Station("Okachimachi", 35.7075185, 139.7748564, line);
+        akihabara = new Station("Akihabara", 35.698383, 139.7730717, line);
+        kanda = new Station("Kanda", 35.6918216, 139.7709318, line);
+        tokyo = new Station("Tokyo", 35.6812362, 139.7649308, line);
+        yurakucho = new Station("Yurakucho", 35.6749187, 139.7628199, line);
+        shimbashi = new Station("Shimbashi", 35.666379, 139.7583398, line);
+        hamamatsucho = new Station("Hamamatsucho", 35.6553809, 139.7571289, line);
+        tamachi = new Station("Tamachi", 35.6457361, 139.7475624, line);
+        shinagawa = new Station("Shinagawa", 35.6284713, 139.7387597, line);
+        osaki = new Station("Osaki", 35.6198513, 139.7281892, line);
+        gotanda = new Station("Gotanda", 35.6261591, 139.7236022, line);
+        merugo = new Station("Meguro", 35.6340929, 139.7158331, line);
+        ebisu = new Station("Ebisu", 35.6467139, 139.7100777, line);
+        shibuya = new Station("Shibuya", 35.6580339, 139.7016358, line);
+        harajuku = new Station("Hara+++juku", 35.6702285, 139.7026976, line);
+        yoyogi = new Station("Yoyogi", 35.683033, 139.7020555, line);
+        shinjuku = new Station("Shinjuku", 35.6896067, 139.7005713, line);
+        shinOkubo = new Station("Shin-Okubo", 35.7012459, 139.7002258, line);
+//YELLOW LINE
+        shinjuku.addLine(line = "yellow");
+        yoyogi.addLine(line);
+        sendagaya = new Station("Sendagaya", 35.6811956, 139.7112808, line);
+        shinanomachi = new Station("Shinanomachi", 35.6800602, 139.7203199, line);
+        yotsuya = new Station("Yotsuya", 35.6861525, 139.7302183, line);
+        iichigaya = new Station("Ichigaya", 35.6910121, 139.7355674, line);
+        iidabashi = new Station("Lidabashi", 35.7020837, 139.7450232, line);
+        suidobashi = new Station("Suidobashi", 35.7020484, 139.7535016, line);
+        akihabara.addLine(line);
+
+//RED LINE         
+        tokyo.addLine(line = "red");
+        ochanomizu = new Station("Ochanomizu", 35.6993854, 139.7652479, line);
+        yoyogi.addLine(line);
+        shinjuku.addLine(line);
+
+        Station[] stations = {shinOkubo, takadanobaba, mejiro, ikebukuro, otsuka, sugamo, komagome, tabata, nishiNippori, nippori,
+            uguisudani, ueno, okachimachi, akihabara, kanda, tokyo, yurakucho, shimbashi, hamamatsucho, tamachi, shinagawa, osaki,
+            gotanda, merugo, ebisu, shibuya, harajuku, yoyogi, shinjuku, ochanomizu, sendagaya, shinanomachi, yotsuya, iichigaya,
+            iidabashi, suidobashi};
+        this.stations = new ArrayList<Station>(Arrays.asList(stations));
+
+    }
+
+    public void createGraph() {
+        //TO CALCULATE PENALTY WE CHOOSE AVERAGE TIME THAT IS LESS OF 4MINS BETWEEN STATIONS
+        final double penalty = 3;
+        //WE CREATE A NODE FOR EACH STATION
+        for (Station s : this.stations) {
+            metro.addNode(new Node(s));
+        }
+
+        //WE CREATE ALL THE CONNECTIONS
+        //change 5 for real distances (h)
+        ArrayList<Edge> eAux = new ArrayList() {
+            {
+
+                //GREEN LINE
+                add(new Edge(metro.getNode(shinOkubo), metro.getNode(takadanobaba), 1.4));
+                add(new Edge(metro.getNode(takadanobaba), metro.getNode(mejiro), 0.9));
+                add(new Edge(metro.getNode(mejiro), metro.getNode(ikebukuro), 1.2));
+                add(new Edge(metro.getNode(ikebukuro), metro.getNode(otsuka), 1.8));
+                add(new Edge(metro.getNode(otsuka), metro.getNode(sugamo), 1.1));
+                add(new Edge(metro.getNode(sugamo), metro.getNode(komagome), 0.7));
+                add(new Edge(metro.getNode(komagome), metro.getNode(tabata), 1.6));
+                add(new Edge(metro.getNode(tabata), metro.getNode(nishiNippori), 0.8));
+                add(new Edge(metro.getNode(nishiNippori), metro.getNode(nippori), 0.5));
+                add(new Edge(metro.getNode(nippori), metro.getNode(uguisudani), 1.1));
+                add(new Edge(metro.getNode(uguisudani), metro.getNode(ueno), 1.1));
+                add(new Edge(metro.getNode(ueno), metro.getNode(okachimachi), 0.6));
+                add(new Edge(metro.getNode(okachimachi), metro.getNode(akihabara), 1));
+                add(new Edge(metro.getNode(akihabara), metro.getNode(kanda), 0.7));
+                add(new Edge(metro.getNode(kanda), metro.getNode(tokyo), 1.3));
+                add(new Edge(metro.getNode(tokyo), metro.getNode(yurakucho), 0.8));
+                add(new Edge(metro.getNode(yurakucho), metro.getNode(shimbashi), 1.1));
+                add(new Edge(metro.getNode(shimbashi), metro.getNode(hamamatsucho), 1.2));
+                add(new Edge(metro.getNode(hamamatsucho), metro.getNode(tamachi), 1.5));
+                add(new Edge(metro.getNode(tamachi), metro.getNode(shinagawa), 2.2));
+                add(new Edge(metro.getNode(shinagawa), metro.getNode(osaki), 2));
+                add(new Edge(metro.getNode(osaki), metro.getNode(gotanda), 0.9));
+                add(new Edge(metro.getNode(gotanda), metro.getNode(merugo), 1.2));
+                add(new Edge(metro.getNode(merugo), metro.getNode(ebisu), 1.5));
+                add(new Edge(metro.getNode(ebisu), metro.getNode(shibuya), 1.6));
+                add(new Edge(metro.getNode(shibuya), metro.getNode(harajuku), 1.2));
+                add(new Edge(metro.getNode(harajuku), metro.getNode(yoyogi), 1.5));
+                add(new Edge(metro.getNode(yoyogi), metro.getNode(shinjuku), 0.7));
+                add(new Edge(metro.getNode(shinjuku), metro.getNode(shinOkubo), 1.3));
+
+                //Yellow
+                add(new Edge(metro.getNode(suidobashi), metro.getNode(iidabashi), 0.9));
+                add(new Edge(metro.getNode(iidabashi), metro.getNode(iichigaya), 1.5));
+                add(new Edge(metro.getNode(iichigaya), metro.getNode(shinanomachi), 2.1));
+                add(new Edge(metro.getNode(shinanomachi), metro.getNode(sendagaya), 0.7));
+                add(new Edge(metro.getNode(suidobashi), metro.getNode(ochanomizu), penalty));//WIKIPEDIA SAYS THAT
+
+            }
+        };
+
+        //EACH STATION ITS GOING TO ADD EACH EDGE WHERE IT´S CONCIDERED AS POINT
+        for (Node N : metro.getNodes()) {
+            for (Edge E : eAux) {
+                if ((E.getNodes()[0] == N) || E.getNodes()[1] == N) {
+                    N.addEdge(E);
+                } else {
+                }
+            }
+        }
+
+//INTERCHANGE STATIONS
+        Node f1, f2, f3, auxN, auxN1; //FICTICIUS AND AUXILIAR NODES
+
+        //TOKYO STATION ON GREEN LINE
+        //CAMBIO DE LINEA GREEN TO RED
+        Edge aux = new Edge(f1 = new Node(tokyo), auxN = metro.getNode(tokyo), penalty);
+        //ADD EDGE TO TOKYO- FICTICIUS 1(TOKYO)
+        f1.addEdge(aux);
+        auxN.addEdge(aux);
+
+        //ADD EDGE TO  FICTICIUS 1(TOKYO)-OCHANOMIZU WITH REAL DISTANCE
+        aux = new Edge(f1, auxN = metro.getNode(ochanomizu), 2.6);
+
+        f1.addEdge(aux);
+        auxN.addEdge(aux);
+        metro.addNode(f1);
+
+        //SHINJUKU STATION ON GREEN LINE
+        //CAMBIO DE LINEA GREEN TO RED
+        aux = new Edge(f1 = new Node(shinjuku), auxN = metro.getNode(shinjuku), penalty);
+        //ADD EDGE TO SHINJUKU- FICTICIUS 1 
+        f1.addEdge(aux);
+        auxN.addEdge(aux);
+        //ADD EDGE TO SHINJUKU- FICTICIUS 1-OCHANOMIZU WITH REAL DISTANCE
+        aux = new Edge(f1, auxN1 = metro.getNode(ochanomizu), 1.5);
+        f1.addEdge(aux);
+        auxN1.addEdge(aux);
+
+        metro.addNode(f1);
+
+        //CAMBIO DE LINEA GREEN TO YELLOW
+        aux = new Edge(f2 = new Node(shinjuku), auxN, penalty);
+        //ADD EDGE TO SHINJUKU- FICTICIUS 2 
+        f2.addEdge(aux);
+        auxN.addEdge(aux);
+        metro.addNode(f2);
+
+        //YOYOGY STATION ON GREEN LINE
+        //CAMBIO DE LINEA GREEN TO YELLOW
+        aux = new Edge(f3 = new Node(yoyogi), auxN = metro.getNode(yoyogi), penalty);
+        //ADD EDGE TO SHINJUKU- FICTICIUS 3 
+        f3.addEdge(aux);
+        auxN.addEdge(aux);
+
+        aux = new Edge(f1, f2, penalty);
+        f2.addEdge(aux);
+        f1.addEdge(aux);
+
+        //ADD EDGE YOYOGY- FICTICIUS 3 - FICTICIUS 2 - SHINJUKU
+        aux = new Edge(f3, f2, 0.7); //REAL DISTANCE YOYOGY-SHINJUKU YELLOE LINE
+        f3.addEdge(aux);
+        f2.addEdge(aux);
+        //metro.addNode(f1);
+        // metro.addNode(f2);
+        //ADD EDGE YOYOGY- FICTICIUS 3 - sendagaya
+        aux = new Edge(f3, auxN = metro.getNode(sendagaya), 1);//REAL DISTANCE YOYOGY-SENDAGAYA YELLOE LINE
+        f3.addEdge(aux);
+        auxN.addEdge(aux);
+        metro.addNode(f3);
+
+        //AKIHABARA STATION ON GREEN LINE
+        //CAMBIO DE LINEA GREEN TO YELLOW
+        aux = new Edge(f1 = new Node(akihabara), auxN = metro.getNode(akihabara), penalty);
+        //ADD EDGE TO AKIHABARA - FICTICIUS 1 
+        f1.addEdge(aux);
+        auxN.addEdge(aux);
+        //ADD EDGE TO AKIHABARA - FICTICIUS 1-SUIDOBASHI WITH REAL DISTANCE
+        Edge aux1 = new Edge(f1, auxN = metro.getNode(suidobashi), 1.5);
+        f1.addEdge(aux1);
+        auxN.addEdge(aux1);
+        metro.addNode(f1);
+
+        for (Node N : metro.getNodes()) {
+            System.out.println(N.getStation().getName() + ":");
+            for (Edge E : N.getEdges()) {
+                if (E.getNodes()[0].equals(N)) {
+                    System.out.println("    -" + (E.getNodes()[1]).getStation().getName() + ": " + E.getDistance());
+                } else {
+                    System.out.println("    -" + (E.getNodes()[0]).getStation().getName() + ": " + E.getDistance());
+                }
+            }
+
+        }
+        System.out.println();
+
+    }
+
+   /* public static void main(String[] args) {
+        Metro T = new Metro();
+        T.createStations();
+        T.createGraph();
+        StarA A = new StarA();
+        A.starASearch(T.getMetro().getNode(T.ueno), T.getMetro().getNode(T.suidobashi));
+       
+    }*/
+
+}
